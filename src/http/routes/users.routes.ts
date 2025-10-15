a90d308c9ae7ae567f5a6bb5899c292a9d7d771f
mattheus-jordany <mattheusjordany2004@gmail.com>
2025-10-15 00:47:19 -0300

    ✨ feat: Implementa rota de login com geração de token JWT

--- src/http/routes/users.routes.ts
+++ src/http/routes/users.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import { CreateUserController } from '../controllers/users/CreateUserController.js';
+import { AuthenticateUserController } from '../controllers/users/AuthenticateUserController.js';
 
 const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
+const authenticateUserController = new AuthenticateUserController();
 
 usersRoutes.post('/', createUserController.handle);
+
+usersRoutes.post('/sessions', authenticateUserController.handle);
 
 export { usersRoutes };
