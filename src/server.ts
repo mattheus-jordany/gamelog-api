97458cea6a2aa71ead3353c1201623b4b4b4ef5e
mattheus-jordany <mattheusjordany2004@gmail.com>
2025-10-22 02:00:54 -0300

    🔧 chore: Move a porta do servidor para variável de ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,19 @@
 import express from 'express';
 import { usersRoutes } from './http/routes/users.routes.js';
+import { gamesRoutes } from './http/routes/games.routes.js';
 
 const app = express();
 app.use(express.json());
 
 app.use('/users', usersRoutes);
+app.use('/games', gamesRoutes);
 
-// Uma rota de teste simples para verificar se o servidor está no ar
 app.get('/', (request, response) => {
   return response.json({ message: 'GameLog API is running!' });
 });
  
-// Define a porta em que o servidor vai rodar
-const PORT = 8080;
+const PORT = process.env.PORT;
 
-// Inicia o servidor
 app.listen(PORT, () => {
   console.log(`🚀 Servidor esta rodando na porta ${PORT}`);
 });
