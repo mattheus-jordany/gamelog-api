7ae48355788b30ad28468a4d590fea6739ed304d
mattheus-jordany <mattheusjordany2004@gmail.com>
2025-11-09 20:26:02 -0300

    ✨ feat: Adiciona endpoint para detalhar um jogo por ID

--- src/http/routes/games.routes.ts
+++ src/http/routes/games.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { CreateGameController } from '../controllers/games/CreateGameController.js';
 import { ListGamesController } from '../controllers/games/ListGamesController.js';
+import { GetGameByIdController } from '../controllers/games/GetGameByIdController.js';
+
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated.js';
 import { ensureAdmin } from '../middlewares/ensureAdmin.js';
 
@@ -9,8 +11,12 @@ const gamesRoutes = Router();
 const createGameController = new CreateGameController();
 const listGamesController = new ListGamesController();
 
+const getGameByIdController = new GetGameByIdController();
+
 gamesRoutes.post('/', ensureAuthenticated, ensureAdmin, createGameController.handle);
 
 gamesRoutes.get('/', listGamesController.handle);
+
+gamesRoutes.get('/:id', getGameByIdController.handle);
 
 export { gamesRoutes };
