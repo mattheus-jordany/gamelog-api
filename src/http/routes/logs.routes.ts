7c924f28176f797c08290c4ba32305f7ec6f7ec6
mattheus-jordany <mattheusjordany2004@gmail.com>
2025-11-09 21:22:43 -0300

    ✨ feat: Adiciona endpoint para usuário listar seus próprios logs

--- src/http/routes/logs.routes.ts
+++ src/http/routes/logs.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 
 import { CreateUserGameLogController } from '../controllers/logs/CreateUserGameLogController.js';
+import { ListUserGameLogsController } from '../controllers/logs/ListUserGameLogsController.js';
+
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated.js';
 
 const logsRoutes = Router();
 
 const createUserGameLogController = new CreateUserGameLogController();
+const listUserGameLogsController = new ListUserGameLogsController();
 
 logsRoutes.post('/', ensureAuthenticated, createUserGameLogController.handle);
+logsRoutes.get('/my', ensureAuthenticated, listUserGameLogsController.handle);
 
 export { logsRoutes };
