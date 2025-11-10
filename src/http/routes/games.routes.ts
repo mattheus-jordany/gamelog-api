1ec8ee534423de5d5850ffd3fb9f13f24abaae25
mattheus-jordany <mattheusjordany2004@gmail.com>
2025-11-09 21:04:03 -0300

    ✨ feat: Adiciona endpoint protegido para admins deletarem jogos

--- src/http/routes/games.routes.ts
+++ src/http/routes/games.routes.ts
@@ -4,6 +4,7 @@ import { CreateGameController } from '../controllers/games/CreateGameController.
 import { ListGamesController } from '../controllers/games/ListGamesController.js';
 import { GetGameByIdController } from '../controllers/games/GetGameByIdController.js';
 import { UpdateGameController } from '../controllers/games/UpdateGameController.js';
+import { DeleteGameController } from '../controllers/games/DeleteGameController.js';
 
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated.js';
 import { ensureAdmin } from '../middlewares/ensureAdmin.js';
@@ -14,6 +15,7 @@ const createGameController = new CreateGameController();
 const listGamesController = new ListGamesController();
 const getGameByIdController = new GetGameByIdController();
 const updateGameController = new UpdateGameController();
+const deleteGameController = new DeleteGameController();
 
 gamesRoutes.post('/', ensureAuthenticated, ensureAdmin, createGameController.handle);
 
@@ -22,5 +24,7 @@ gamesRoutes.get('/', listGamesController.handle);
 gamesRoutes.get('/:id', getGameByIdController.handle);
 
 gamesRoutes.put('/:id', ensureAuthenticated, ensureAdmin, updateGameController.handle);
+
+gamesRoutes.delete('/:id', ensureAuthenticated, ensureAdmin, deleteGameController.handle);
 
 export { gamesRoutes };
