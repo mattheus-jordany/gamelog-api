6bf7608bf4430d479697ad1f69b8df9764395b3a
mattheus-jordany <mattheusjordany2004@gmail.com>
2025-11-09 22:08:50 -0300

    ✨ feat: Adiciona endpoint para usuário deletar o próprio perfil

--- src/http/controllers/auth/ForgotPasswordController.ts
+++ src/http/controllers/auth/ForgotPasswordController.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
 import crypto from 'crypto';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
