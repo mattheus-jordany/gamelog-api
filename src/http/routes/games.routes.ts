a5bc8f99dd45e8b7826e6089c69db928b23051dc
mattheus-jordany <mattheusjordany2004@gmail.com>
2025-11-18 22:24:22 -0300

    ✨ feat: Adiciona upload de capa de jogo com Cloudinary

--- src/http/routes/games.routes.ts
+++ src/http/routes/games.routes.ts
@@ -5,6 +5,9 @@ import { ListGamesController } from '../controllers/games/ListGamesController.js
 import { GetGameByIdController } from '../controllers/games/GetGameByIdController.js';
 import { UpdateGameController } from '../controllers/games/UpdateGameController.js';
 import { DeleteGameController } from '../controllers/games/DeleteGameController.js';
+import { UploadGameCoverController } from '../controllers/games/UploadGameCoverController.js';
+
+import { upload } from '../../config/multer.js';
 
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated.js';
 import { ensureAdmin } from '../middlewares/ensureAdmin.js';
@@ -16,6 +19,7 @@ const listGamesController = new ListGamesController();
 const getGameByIdController = new GetGameByIdController();
 const updateGameController = new UpdateGameController();
 const deleteGameController = new DeleteGameController();
+const uploadGameCoverController = new UploadGameCoverController();
 
 gamesRoutes.post('/', ensureAuthenticated, ensureAdmin, createGameController.handle);
 
@@ -26,5 +30,7 @@ gamesRoutes.get('/:id', getGameByIdController.handle);
 gamesRoutes.put('/:id', ensureAuthenticated, ensureAdmin, updateGameController.handle);
 
 gamesRoutes.delete('/:id', ensureAuthenticated, ensureAdmin, deleteGameController.handle);
+
+gamesRoutes.patch('/:id/cover', ensureAuthenticated, ensureAdmin, upload.single('cover'), uploadGameCoverController.handle);
 
 export { gamesRoutes };
