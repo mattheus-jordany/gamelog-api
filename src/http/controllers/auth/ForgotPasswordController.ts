3f89a75f30f411c043664a1cf055d2001e9a24e5
mattheus-jordany <mattheusjordany2004@gmail.com>
2025-11-18 23:00:47 -0300

    ✨ feat: Implementa lógica e rota para solicitação de recuperação de senha

--- src/http/controllers/auth/ForgotPasswordController.ts
+++ src/http/controllers/auth/ForgotPasswordController.ts
@@ -1,8 +1,10 @@
 import type { Request, Response } from 'express';
 import crypto from 'crypto';
+import { EtherealMailProvider } from '../../../shared/provider/MailProvider/EtherealMailProvider.js';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
+const mailProvider = new EtherealMailProvider();
 
 export class ForgotPasswordController {
   async handle(request: Request, response: Response) {
@@ -32,19 +34,20 @@ export class ForgotPasswordController {
         },
       });
 
-      // 4. SIMULAÇÃO DO ENVIO DE E-MAIL
-      console.log('=================================');
-      console.log('SIMULAÇÃO DE ENVIO DE E-MAIL:');
-      console.log(`Para: ${email}`);
-      console.log(`Token de recuperação: ${resetToken}`);
-      console.log('=================================');
+      const recoveryLink = `http://localhost:8080/reset-password?token=${resetToken}`;
+      
+      await mailProvider.sendMail(
+        email,
+        'Recuperação de Senha - GameLog API',
+        `Olá, use este link para redefinir sua senha: <a href="${recoveryLink}">${recoveryLink}</a>`
+      );
 
       return response.status(200).json({
         message: 'Se um usuário com este e-mail existir, um link de recuperação será enviado.',
       });
 
     } catch (error) {
-      console.error(error);
+      console.error('ERRO NO FLUXO DE ESQUECI MINHA SENHA:', error);
       return response.status(500).json({ error: 'Internal server error' });
     }
   }
